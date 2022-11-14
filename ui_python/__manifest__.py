cf3e7c293212946e385329145bba32d9294f6b87
mohanadhamdan991 <118097237+mohanadhamdan991@users.noreply.github.com>
2022-11-14 13:09:56 +0200

    Update __manifest__.py
    [FIX] version ui_python

--- ui_python/__manifest__.py
+++ ui_python/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "User interface Python",
-    'version': '13.0.1',
+    'version': '13.0.2',
     'summary': 'Execute Python Codes',
     'sequence': -100,
     'description': """Execute Python Codes""",
