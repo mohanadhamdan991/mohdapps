d4c61a6193ec1c7dcb31150ba3a3715a78257183
mohanadhamdan991 <118097237+mohanadhamdan991@users.noreply.github.com>
2022-11-14 13:20:25 +0200

    Add files via upload
    [ADD] ui_python

--- ui_python/__manifest__.py
+++ ui_python/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "User interface Python",
-    'version': '13.0.2',
+    'version': '16.0.2',
     'summary': 'Execute Python Codes',
     'sequence': -100,
     'description': """Execute Python Codes""",
