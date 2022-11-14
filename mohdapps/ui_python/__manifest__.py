c936c451ee0aece262ed9de552cce4d1614cc0d6
mohanadhamdan991 <118097237+mohanadhamdan991@users.noreply.github.com>
2022-11-14 11:34:37 +0200

    Add files via upload
    [Add] ui_python

---
+++ mohdapps/ui_python/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "User interface Python",
+    'version': '13.0.1',
+    'summary': 'Execute Python Codes',
+    'sequence': -100,
+    'description': """Execute Python Codes""",
+    'category': 'Extra Tools',
+    'author': "Mohannad Hamdan",
+    'maintainer': 'Mohannad Hamdan',
+    'price': '5.0',
+    'currency': 'USD',
+    'website': "https://www.hamdanerp.com",
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [
+        'views/ui_python.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'qweb': [],
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
