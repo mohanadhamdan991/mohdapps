a13da69c738b3a25bc4ee8b0204ef0611a79bd06
mohanadhamdan991 <118097237+mohanadhamdan991@users.noreply.github.com>
2022-11-28 16:03:27 +0300

    Add files via upload
    [ADD] odoo_whatsapp_web

---
+++ odoo_whatsapp_web/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Whatsapp Web",
+    'version': '13.0.1',
+    'summary': 'This module is used to send msg on whatsapp web',
+    'sequence': -100,
+    'description': """This module is used to send msg on whatsapp web""",
+    'category': 'Discuss',
+    'author': "HamdanERP",
+    'maintainer': 'HamdanERP',
+    'price': '2.99',
+    'currency': 'USD',
+    'website': 'https://www.hamdanerp.com',
+    'license': 'AGPL-3',
+    'depends': ['mail', 'account', 'base'],
+    'data': [
+        'views/odoo_whatsapp_web.xml',
+        'views/whatsapp_icon_chat.xml',
+    ],
+    'images': ['static/description/banner6.gif'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
